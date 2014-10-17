07f6106dcefec7b69dfd5f46cf4bbf2b29904428
imadige <imadige@gmail.com>
2014-10-17 13:53:45 +0300

    Revert "MVC"
    This reverts commit f2ccbe2c632827de6f9e6acfa9406837593fa5ed.

--- view/layouts/column1.php
+++
@@ -1,7 +0,0 @@
-<?php /* @var $this Controller */ ?>
-
-<?php Smce::content('//layouts/header'); ?>
-<div id="content">
-	<?php echo $content; ?>
-</div><!-- content -->
-<?php Smce::content('//layouts/footer'); ?>
