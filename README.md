09063d1bbdf2cefd746920e4a1c5972e56ccd053
imadige <imadige@gmail.com>
2014-10-24 13:48:29 +0300

    MVC
    MVC

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-version 1.0.5
+﻿version 1.0.6
 
 PHP-TR gurubu https://www.facebook.com/groups/tr.developers/
 Samed Ceylan
--- demo/front/js/menu.js
+++ demo/front/js/menu.js
@@ -58,7 +58,7 @@
 
       });
   };
-})(jQuery);
+});
 
 (function($){
 $(document).ready(function(){
@@ -107,4 +107,4 @@ function() {
 
 
 });
-})(jQuery);
+});
