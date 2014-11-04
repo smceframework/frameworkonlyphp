b72c107a67dbf9a8bcc9a1514eeb44a9f9b62fa6
imadige <imadige@gmail.com>
2014-11-04 10:03:34 +0200

    MVC
    MVC

--- smceframework/base/Sm_Controller.php
+++ smceframework/base/Sm_Controller.php
@@ -11,25 +11,22 @@ class Sm_Controller extends \Sm_Base\Sm_Ve
 	
 	const COMPENENTS="Controller";
 	
+	private $viewFile="";
+	private $layoutFile="";
+	
     public function render($url="",$array=array())
     {
-        ob_start();
-        if (! is_file(\Smce::app()->basePath.(empty($this->theme) ? "\\" : "\\theme\\".$this->theme."\\")."view\\".(self::$error==true ? BASE_CONTROLLER : "site")."\\".$url.".php")) {
-                $html = '<html><body><h1>View Not Found "'.(empty($this->theme) ? "\\" : "theme\\".$this->theme."\\")."view\\".(self::$error==true ? BASE_CONTROLLER : "site")."\\".$url.'"</h1></body></html>';
-                echo $html;
-                exit;
-        } else {
-             extract($array);
-             include (\Smce::app()->basePath.(empty($this->theme) ? "\\" : "\\theme\\".$this->theme."\\")."view\\".(self::$error==true ? BASE_CONTROLLER : "site")."\\".$url.".php");
-        }
-        $this->content = ob_get_contents();
-        ob_end_clean();
-		$this->settingRender();
+		$this->setViewFile($url);
+		$this->setLayoutFile();
+		$this->settingRender($array);
 
     }
 	
-	private function settingRender(){
+	
+	
+	private function settingRender($array){
 		$this->setTheme();
+		$this->adjustmentView($array);
 		$this->adjustmentLayout();
 	}
 	
@@ -37,30 +34,70 @@ class Sm_Controller extends \Sm_Base\Sm_Ve
 		 define("BASE_THEME",$this->theme);
 	}
 	
+	private function adjustmentView($array){
+		ob_start();
+        if (! is_file($this->viewFile)){
+                $html = '<html><body><h1>View Not Found "'.$this->viewFile.'"</h1></body></html>';
+                echo $html;
+                exit;
+        } else {
+             extract($array);
+             include ($this->viewFile);
+        }
+        $this->content = ob_get_contents();
+        ob_end_clean();
+	}
+	
 	private function adjustmentLayout(){
 		
 		$content=$this->content;
 		
-		if (!empty($this->layout)) {
-			$layout=$this->layout;
-		} elseif (class_exists($components) && isset(\Controller::$layout)) {
-			$layout=\Controller::$layout;
-		}
-		
-		 if (empty($layout)) {
-		
+		 if (empty($this->layoutFile)) {
 				$html = '<html><body><h1>Not Set Layout</h1></body></html>';
 				echo $html;
 				exit;
-		} elseif (!is_file(\Smce::app()->basePath.(empty($this->theme) ? "\\" : "\\theme\\".$this->theme."\\")."view".$layout.".php")) {
+		} elseif (!is_file($this->layoutFile)) {
 		
-				$html = '<html><body><h1>Layout Not Found "'.(empty($this->theme) ? "\\" : "theme\\".$this->theme."\\")."view\\".$layout.'"</h1></body></html>';
+				$html = '<html><body><h1>Layout Not Found "'.$this->layoutFile.'"</h1></body></html>';
 				echo $html;
 				exit;
 		} else {
 		
-				include (\Smce::app()->basePath.(empty($this->theme) ? "\\" : "\\theme\\".$this->theme."\\")."view".$layout.".php");
+				include ($this->layoutFile);
 		}
+	}
+	
+	
+	private function setViewFile($url){
+		
+		$this->viewFile=\Smce::app()->basePath."\\";
+		if(!empty($this->theme)){
+			$this->viewFile.="theme\\".$this->theme."\\";
+		}
+		$this->viewFile.="view\\";
+		if(self::$error==true){
+			$this->viewFile.=BASE_CONTROLLER."\\";
+		}else{
+			$this->viewFile.="site\\";
+		}
+		
+		$this->viewFile.=$url.".php";
+	}
+	
+	private function setLayoutFile(){
+		$layout="";
+		if (!empty($this->layout)) {
+			$layout=$this->layout;
+		} elseif (class_exists(self::COMPENENTS) && isset(\Controller::$layout)) {
+			$layout=\Controller::$layout;
+		}
+		
+		$this->layoutFile=\Smce::app()->basePath."\\";
+		if(!empty($this->theme)){
+			$this->layoutFile.="theme\\".$this->theme."\\";
+		}
+		$this->layoutFile.="view";
+		$this->layoutFile.=$layout.".php";
 	}
 
 }
