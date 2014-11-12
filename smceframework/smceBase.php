8bc0d403c4703a47259c66eddc62e93af6b6eef2
Mehmet SOYLU <mssoylu@gmail.com>
2014-11-12 05:22:56 +0200

    Update smceBase.php

--- smceframework/smceBase.php
+++ smceframework/smceBase.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * @todo dosya ve degisken isimlerini okunur kilmaz icin SMCE prefixler kaldirilacak
+ * @todo dosyalar ve degiskenler gorev ve iceriklerine gore isimlendirilecek.
+ */
 
 /*framework base path*/
 define('SMCE_BASE_PATH',dirname(__FILE__));
