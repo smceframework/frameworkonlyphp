10ad734160e06d713f00de7e34c81023a5276899
Mehmet SOYLU <mssoylu@gmail.com>
2014-11-12 05:18:17 +0200

    Update DB.php

--- smceframework/base/SmLib/DB.php
+++ smceframework/base/SmLib/DB.php
@@ -16,6 +16,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/**
+ * @todo bu dosya dahil tum dosyalarda birden fazla class olamyacak
+ */
 class DB
 {
   // initial connection
