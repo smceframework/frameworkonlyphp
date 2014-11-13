053e96767193e369a955431a60699db33eb93381
imadige <imadige@gmail.com>
2014-11-13 17:27:10 +0200

    update
    update

--- demo/view/layouts/column1.php
+++
@@ -1,6 +0,0 @@
-<?php Smce::theme()->content('//layouts/header',array()); ?>
-
-<div id="content">
-	<?php echo $content; ?>
-</div><!-- content -->
-<?php Smce::theme()->content('//layouts/footer',array()); ?>
--- smceframework/amp/SMUserIdentityImp.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace smce\amp;
-
-interface SmUserIdentityImp
-{
-	public function authenticate();
-
-}
