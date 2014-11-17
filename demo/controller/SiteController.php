ac64d90baead07ee259d2e6b5a3a98bf60155db7
imadige <imadige@gmail.com>
2014-11-17 13:57:03 +0200

    update
    update

---
+++ demo/controller/SiteController.php
@@ -0,0 +1,71 @@
+<?php
+
+use Smce\Core\SmController;
+
+
+class SiteController extends SmController
+{
+
+    public $layout='//layouts/column1';
+
+    public function actionIndex()
+    {
+		
+        if (Smce::app()->getState("name")=="") {
+             $this->render("index",array(
+                "model"=>"Site controllerden gelen bi yazıdır",
+                "grup"=>"PHP-TR grubu",
+                "grup_site"=>"https://www.facebook.com/groups/tr.developers/",
+             ));
+        } else {
+            $this->redirect("panel/index");
+        }
+    }
+	
+
+    public function actionAbout()
+    {
+         $this->render("pages/about");
+    }
+
+	
+    public function actionLogin()
+    {
+		
+        $model=new LoginForm();
+
+        if (isset($_POST["LoginForm"])) {
+            $post=(object) $_POST["LoginForm"];
+
+            $model->username    =    $post->username;
+            $model->password    =    $post->password;
+			if(isset($post->rememberMe))
+            	$model->rememberMe    =  $post->rememberMe;
+			
+            if ($model->validate() && $model->login()) {
+			
+                //redirect url
+                $this->redirect("panel/index");
+
+            }
+        }
+
+        $this->render("login",array(
+            "model"=>$model,
+         ));
+
+    }
+
+    public function actionLogout()
+    {
+        Smce::app()->stateClear();
+        $this->redirect("site/index");
+    }
+
+    public function error($err)
+    {
+         $this->render("error",array(
+            "code"=>$err,
+         ));
+    }
+}
---
+++ smceframework/amp/SMUserIdentityImp.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace smce\amp;
+
+interface SmUserIdentityImp
+{
+	public function authenticate();
+
+}
