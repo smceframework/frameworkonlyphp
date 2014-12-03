cd10f1fae2ef3b259a2c2d3d13fee316c65941cd
İrfan Evrens <irfanevrens@gmail.com>
2014-12-03 15:46:57 +0200

    ufak değişiklikler

--- smceframework/core/SmController.php
+++ smceframework/core/SmController.php
@@ -10,7 +10,7 @@ class SmController extends SmVe
 {
     private $content;
 	
-    public $theme="";
+    public $theme = "";
 	
 	public $layout="";
 	
@@ -18,7 +18,7 @@ class SmController extends SmVe
 
 	const COMPENENTS="Controller";
 
-    public function render($view="",$array=array())
+    public function render($view = "", $array = array())
     {
 		$controller=strtolower(get_class($this));
 		$controller=str_replace("controller","",$controller);
@@ -28,40 +28,38 @@ class SmController extends SmVe
 		$SmTemplate->setLayout($this->getLayout());
 		$SmTemplate->setThemeDirectory($this->getTheme());
 		$SmTemplate->run();
-			
     }
 	
-	private function getLayout(){
-		if($this->layout!="")
+	private function getLayout() 
+	{
+		if ($this->layout != "")
 			return $this->layout;
 	    elseif (class_exists(self::COMPENENTS) && isset(\Controller::$layout))
 			return \Controller::$layout;	
 	}
 	
-	private function getTheme(){
-		if(empty($this->theme))
-			return BASE_PATH;
-		else
-			return BASE_PATH."/theme/".$this->theme;
+	private function getTheme() 
+	{
+		return empty($this->theme) ? 
+			BASE_PATH : 
+			BASE_PATH . "/theme/" . $this->theme;
 	}
 	
-	private function getView($view,$controller)
+	private function getView($view, $controller)
 	{
-		if(is_array($view)){
-			$view=explode("/",$view[0]);
-			$view= $view[0]."/".$view[1];
-		}elseif (!is_array($view)) {
-			$view= $controller."/".$view;
+		if (is_array($view)) {
+			$view = explode("/", $view[0]);
+			$view = $view[0] . "/" . $view[1];
+		} elseif ( ! is_array($view)) {
+			$view = $controller . "/" . $view;
 		} else {
-			$view= "site/".$view[0];
+			$view = "site/" . $view[0];
 		}
 		return $view;
 	}
 
 	private function setTheme()
 	{
-		 define("BASE_THEME",$this->theme);
+		 define("BASE_THEME", $this->theme);
 	}
-
-	
 }
