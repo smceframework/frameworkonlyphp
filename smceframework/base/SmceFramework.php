6d98eb40513d07b4bc1a352ff337b871c0fd22de
imadige <imadige@gmail.com>
2014-12-05 10:12:12 +0200

    update
    update

--- smceframework/base/SmceFramework.php
+++ smceframework/base/SmceFramework.php
@@ -8,15 +8,42 @@ use Tracy\Debugger;
 
 class SmceFramework
 {
+	private static $smBase;
+	
+	private static $configSmce;
+	
     public static function createWebApplication($config)
     {
+		static::includeFile();
+		
+		static::SmAutoload($config);
+		
+		static::SmTrancy($config);
+		
+		static::appConfig($config);
+		
+		static::smceConfig($config);
+		
+		return self::$smBase;
+    }
+	
+	private static function includeFile()
+	{
 		if(file_exists(SMCE_PATH.'/vendor/autoload.php'))
         	require_once SMCE_PATH.'/vendor/autoload.php';
 			
         require SMCE_PATH."/SmAutoload.php";
+		self::$configSmce=require SMCE_PATH."/config/config.php";
+	}
+	
+	private static function SmAutoload($config)
+	{
 		$SmAutoload = new SmAutoload;
 		$SmAutoload->register($config);
-		
+	}
+	
+	private static function SmTrancy($config)
+	{
 		$SmTracy = new SmTracy;
 		$SmTracy->register();
 		
@@ -26,17 +53,26 @@ class SmceFramework
 			
 		if(!file_exists(BASE_PATH . '/log'))
 			mkdir(BASE_PATH . '/log');
-	
-		Debugger::enable($debug, BASE_PATH . '/log');
 			
+		 Debugger::enable($debug, BASE_PATH . '/log');
+	}
+	
+	private static function appConfig($config)
+	{
 		//App Config
-		$SmBase=new SmBase;
+		self::$smBase=new SmBase;
         SmBase::$config=$config;
-		
+	}
+	
+	private static function smceConfig($config)
+	{
 		//Smce Config
-		$configSmce=require SMCE_PATH."/config/config.php";
 		SmBase::$configSmce=$configSmce;
-        return $SmBase;
-    }
+	}
+	
+	
+	
+	
+	
 	
 }
