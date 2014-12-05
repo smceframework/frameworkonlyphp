748decebb1ef859aa6a1bdbf43771c6efbc19edd
imadige <imadige@gmail.com>
2014-12-05 10:34:32 +0200

    update
    update

--- smceframework/core/SmAccessRules.php
+++ smceframework/core/SmAccessRules.php
@@ -17,24 +17,60 @@ class SmAccessRules
 		foreach ($accessRules as $key => $value) {
 
 			if (isset($value["actions"]) && in_array(strtolower($view), $value["actions"])) {
-
-				if (isset($value["ip"]) && is_array($value["ip"])) {
-
-					return in_array(Smce::app()->IP, $value["ip"]);
-				}
-
-				if ($value["users"] == "@" && Smce::app()->getState("SMCE_login71") == '') {
-
-					SmController::redirect($value["redirect"]);
-				}
-
-				if ($value["expression"] === true) {
-
+				
+				if($this->ipAdressSearch($value["ip"])==true)
 					return true;
-				}
+					
+				$this->loginControl($value["users"],$value["redirect"]);
+
+				if($this->expressionControl($value["expression"])==true)
+					return true;
+					
 			}
 		}
 
 		return false;
+	}
+	
+	/**
+	 * @param $ipAdress
+	 *
+	 * @return bool
+	 */
+	private function ipAdressSearch($ipAdress)
+	{
+		if (!isset($ipAdress) && is_array($ipAdress)) {
+
+			return in_array(Smce::app()->IP, $ipAdress);
+		}
+	}
+	
+	/**
+	 * @param $users
+	 * @param $redirect
+	 *
+	 * @redirect url
+	 */
+	
+	private function loginControl($users,$redirect="")
+	{
+		if (isset($users) && $users == "@" && Smce::app()->getState("SMCE_login71") == '') {
+			
+			SmController::redirect($redirect==""?"site/login":$redirect);
+		}
+	}
+	
+	/**
+	 * @param $expression
+	 *
+	 * @return bool
+	 */
+	
+	private function expressionControl($expression)
+	{
+		if (isset($expression) && $expression === true) {
+
+			return true;
+		}
 	}
 }
