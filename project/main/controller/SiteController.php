2d073241ea1cdca5d4ea5b209afcf5d1a697bd80
Samed Ceylan <imadige@gmail.com>
2014-12-09 21:24:47 +0200

    update
    update

--- project/main/controller/SiteController.php
+++ project/main/controller/SiteController.php
@@ -10,16 +10,8 @@ class SiteController extends SmController
 
     public function actionIndex()
     {
-		
-        if (Smce::app()->getState("name")=="") {
-             $this->render("index",array(
-                "model"=>"Site controllerden gelen bi yazıdır",
-                "grup"=>"PHP-TR grubu",
-                "grup_site"=>"https://www.facebook.com/groups/tr.developers/",
-             ));
-        } else {
-            $this->redirect("panel/index");
-        }
+		$this->render("index");
+        
     }
 	
 
