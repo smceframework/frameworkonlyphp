7a8c74fb19140a085ecb9a43c71a133a58a685ce
imadige <imadige@gmail.com>
2014-12-13 23:58:02 +0200

    update

---
+++ micro/main/controller/SiteController.php
@@ -0,0 +1,36 @@
+<?php
+
+use Smce\Core\SmController;
+use Smce\Lib\SmOutput;
+
+class SiteController extends SmController
+{
+
+    public $layout='//layouts/column1';
+
+    public function actionIndex()
+    {
+    	
+        $hello="Hello World";
+		
+        $this->render("index",array("hello"=>$hello));
+        
+    }
+
+    public function actionOutput()
+	{
+		$SmOutput=new SmOutput;
+		$SmOutput->setContentType("application/json")
+		//->setFileName("hello.json")
+		->put(json_encode(array('message' => 'Hello World!')));
+	}
+
+
+	public function error($err)
+    {
+         $this->render("error",array(
+            "code"=>$err,
+         ));
+    }
+	
+}
