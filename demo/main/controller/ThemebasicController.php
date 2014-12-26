912728d039f916c2b6b7d177e215793f970ecb2e
imadige <imadige@gmail.com>
2014-12-26 09:33:37 +0200

    update
    update

--- demo/main/controller/ThemebasicController.php
+++ demo/main/controller/ThemebasicController.php
@@ -5,7 +5,7 @@ use Smce\Core\SmController;
 class ThemebasicController extends SmController
 {
 
-    public $layout='//layouts/column2';
+    public $layout='//layouts/column1';
     public $theme='basic';
 
     public function actionIndex()
