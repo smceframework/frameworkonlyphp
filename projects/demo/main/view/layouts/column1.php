876bbf87e9fef5ca2f24c5a4c681b04e1bf4d6a8
root <imadige@gmail.com>
2014-12-30 22:13:57 +0200

    update

--- projects/demo/main/view/layouts/column1.php
+++
@@ -1,5 +0,0 @@
-<?php Smce::theme()->content('//layouts/header',array()); ?>
-
-	<?php echo $content; ?>
-
-<?php Smce::theme()->content('//layouts/footer',array()); ?>
--- projects/micro/main/view/site/error403.php
+++
@@ -1,1 +0,0 @@
-<h2>403 Error! <?php echo $message; ?></h2>
